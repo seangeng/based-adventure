382123e1a498c553ba6e8406be18262b17160fbc
Sean Geng <seangeng94@gmail.com>
2024-01-27 14:49:41 -0500

    move to page setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+  return <>{children}</>;
 }
--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,3 +1,22 @@
-export default async function StartScreen() {
-  return <p>Choose your own text based adventure.</p>;
+export default function StartScreen() {
+  return (
+    <html>
+      <head>
+        <title>Mint</title>
+        <meta property="og:title" content="Tested!" />
+        <meta property="og:image" content="/api/welcome" />
+        <meta name="fc:frame" content="vNext" />
+        <meta name="fc:frame:image" content="/api/welcome" />
+        <meta
+          name="fc:frame:post_url"
+          content="https://based-adventure.vercel.app/api/start"
+        />
+        <meta name="fc:frame:button:1" content="Minted to" />
+      </head>
+      <body>
+        <p>Choose your own text-based adventure.</p>
+        {/* other content */}
+      </body>
+    </html>
+  );
 }
