75c9560895c87aabb7dff7c3164624704b037dda
Sean Geng <seangeng94@gmail.com>
2024-01-27 21:31:27 -0500

    add new start screen

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -4,20 +4,14 @@ export default function StartScreen() {
       <head>
         <title>Mint</title>
         <meta property="og:title" content="Tested!" />
-        <meta
-          property="og:image"
-          content="https://based-adventure.vercel.app/api/welcome"
-        />
+        <meta property="og:image" content="/base-quest-start.jpg" />
         <meta name="fc:frame" content="vNext" />
-        <meta
-          name="fc:frame:image"
-          content="https://based-adventure.vercel.app/api/welcome"
-        />
+        <meta name="fc:frame:image" content="/base-quest-start.jpg" />
         <meta
           name="fc:frame:post_url"
           content="https://eo6m4ikat6vrxtj.m.pipedream.net"
         />
-        <meta name="fc:frame:button:1" content="Minted to" />
+        <meta name="fc:frame:button:1" content="Start your adventure" />
       </head>
       <body>
         <p>Choose your own text-based adventure.</p>
