a5c640ad06deac4ed338146d67b66ce0fd6a2976
Sean Geng <seangeng94@gmail.com>
2024-01-30 13:25:20 -0500

    setup profile pages

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,31 +1,44 @@
-export default function Profile({
-  params,
-}: {
-  params: {
-    id: string;
+import type { Metadata, ResolvingMetadata } from "next";
+import { getFrameMetadata } from "@coinbase/onchainkit";
+import { db } from "@/lib/dependencies";
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.id;
+
+  const frameMetadata = getFrameMetadata({
+    buttons: ["Add to party 🤝", "Fight 🤺", "Heal ❤️‍🩹", "Boost 🔼"],
+    image: `${process.env.DOMAIN}/api/profile-image?fid=${id}`,
+    post_url: `${process.env.DOMAIN}/api/profile`,
+  });
+
+  // fetch data
+  const characterState = await db.collection("characters").findOne({ fid: id });
+
+  const profileTitle = `${characterState?.user?.username} • Level ${
+    characterState?.level ?? 1
+  } • ${characterState?.class ?? "Adventurer"} - Base Quest`;
+
+  return {
+    title: profileTitle,
+    description: "AI Powered Text Adventure on Base L2",
+    openGraph: {
+      title: profileTitle,
+      description: "AI Powered Text Adventure on Base L2",
+      images: [`${process.env.DOMAIN}/api/profile-image?fid=${id}`],
+    },
+    other: {
+      ...frameMetadata,
+    },
   };
-}) {
-  return (
-    <html>
-      <head>
-        <title>Profile for {params.id}</title>
-        <meta property="og:title" content={`Profile for ${params.id}`} />
-        <meta
-          property="og:image"
-          content={`${process.env.DOMAIN}/api/profile-image?id=${params.id}`}
-        />
-        <meta name="fc:frame" content="vNext" />
-        <meta
-          name="fc:frame:image"
-          content={`${process.env.DOMAIN}/api/profile-image?id=${params.id}`}
-        />
-        <meta
-          name="fc:frame:post_url"
-          content={`${process.env.DOMAIN}/api/profile?id=${params.id}`}
-        />
-        <meta name="fc:frame:button:1" content="Connect" />
-      </head>
-      <body></body>
-    </html>
-  );
 }
+
+export default function Page({ params, searchParams }: Props) {}
